047c5474ef470cc327e06d2ad3066ca4b35c3986
ybowdotio <office@ybow.io>
2025-08-09 16:42:46 -0500

    Update page.tsx

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -3,8 +3,8 @@
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { supabase } from '@/lib/supabase';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '@/app/components/Header';
+import Footer from '@/app/components/Footer';
 
 type TimelineItem = {
   id: string;
